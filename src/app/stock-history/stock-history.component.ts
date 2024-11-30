a70f6dcaa751cb16d7d2796c68b2b9a2d6d4a123
abhinavguptalko <abhinav.guptalko@gmail.com>
2024-11-30 21:09:59 +0000

    Add files via upload

--- src/app/stock-history/stock-history.component.ts
+++ src/app/stock-history/stock-history.component.ts
@@ -1,7 +1,7 @@
 // src/app/stock-history/stock-history.component.ts
 import { Component, OnInit } from '@angular/core';
 import { StockHistoryService } from '../services/stock-history.service';
-
+import { MatSnackBar } from '@angular/material/snack-bar';  // Import MatSnackBar
 @Component({
   selector: 'app-stock-history',
   templateUrl: './stock-history.component.html',
@@ -12,7 +12,7 @@ export class StockHistoryComponent implements OnInit {
   stockHistory: any[] = [];  // To store the stock history data
   loading: boolean = false;  // Loading state
 
-  constructor(private stockHistoryService: StockHistoryService) { }
+  constructor(private stockHistoryService: StockHistoryService,private snackBar: MatSnackBar) { }
 
   ngOnInit(): void { 
     this.getStockHistory();
@@ -26,10 +26,20 @@ export class StockHistoryComponent implements OnInit {
       },
       error: (error) => {
         console.error('Error fetching stock history', error);
+        const errorMessage = error?.error?.detail || 'Error fetching stock history Please try again.';
+        this.openSnackBar(errorMessage, 'Close', 'error');
       },
       complete: () => {
         this.loading = false;
       }
+    });
+  }
+
+  // Function to open SnackBar
+  openSnackBar(message: string, action: string, panelClass: string): void {
+    this.snackBar.open(message, action, {
+      duration: 3000, // Show message for 3 seconds
+      panelClass: [panelClass] // Add custom class for styling
     });
   }
 }
